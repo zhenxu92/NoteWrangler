4606b44d036c89cfaad0bdec6cb9b65c66f0969d
zhenxu92 <zhenxu0213@gmail.com>
2017-01-04 15:21:15 -0500

    First test on v1.0, first completed version of NoteWrangler

--- app/assets/javascript/vendor/ngGravatar.js
+++ app/assets/javascript/vendor/ngGravatar.js
@@ -2,8 +2,9 @@ angular.module("ngGravatar", []);
 angular.module("ngGravatar").directive("gravatar", function(Gravatar) {
     return {
         replace: true,
-        restrict: 'E',
-        template: "<img ng-src='{{gravatarUrl()}}'",
+        restrict: "E",
+        template: "<img ng-src='{{gravatarUrl()}}'>",
+        // OMG, I missed close tag...
         scope: {email:"="},
         link: function(scope) {
             scope.gravatarUrl = function() {
@@ -14,7 +15,8 @@ angular.module("ngGravatar").directive("gravatar", function(Gravatar) {
 });
 angular.module("ngGravatar").provider('Gravatar', function() {
     var imageSize = 50;
-    var url = "http://www.gravatar.com/avatar";
+    var url = "http://www.gravatar.com/avatar/";
+    // slash at the end is very important, be careful
     
     this.setSize = function(value) {
         imageSize = value;
